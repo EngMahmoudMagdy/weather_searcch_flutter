d4b53a3c224cbe153f743832c9c3bfb42ddca0b1
Magdy <m.magdy@re-vision.nl>
2022-02-10 17:40:30 +0200

    weather bloc added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cubit_bloc_tutorial/pages/weather_search_page.dart';
-import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
 
+import 'bloc/weather_bloc.dart';
 import 'data/weather_repository.dart';
 
 void main() => runApp(MyApp());
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: BlocProvider(
-              create: (context) => WeatherCubit(FakeWeatherRepository()),
-              child: WeatherSearchPage(),
-            ),
+        create: (context) => WeatherBloc(FakeWeatherRepository()),
+        child: WeatherSearchPage(),
+      ),
     );
   }
 }
